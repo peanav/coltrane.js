67a31f9be3cfc6f32b4e7dac0dfe1b4a301ccb7f
peanav <paul.navasard@domo.com>
2013-05-15 22:26:25 -0600

    It can render a bar chart now

--- src/barChart.js
+++ src/barChart.js
@@ -6,22 +6,33 @@ coltrane._barChart = function(ele, opts) {
   typeof(ele) === 'object' ? this.rootElement = ele : this.querySelector = ele;
   if(this.querySelector)
     this.rootElement = document.querySelector(this.querySelector);
+  this.data = coltrane.data.calculate(opts.data || []);
+  this.draw();
 };
 
-coltrane._barChart.prototype.draw = function(ele, data) {
+coltrane._barChart.prototype.draw = function() {
+  var height = 400;
+  var width = 400;
 
-  var x = d3.scale.ordinal().range(0, data.length-1);
-  var y = d3.scale.ordinal().range(_.last(data).percentage);
+  var x = d3.scale.ordinal()
+          .domain(_.map(this.data, "label"))
+          .rangeRoundBands([0, width], .3);
 
-  var svg = d3.select(ele).append("svg")
-              .attr('width', 300)
-              .attr('width', 300)
+  var y = d3.scale.linear()
+          .domain([0, _.max(this.data, "value").value])
+          .range([0, height]);
+
+  this.svg = d3.select(this.rootElement).append("svg")
+              .attr('width', width)
+              .attr('height', height)
             .append('g');
 
-  svg.selectAll('.bar')
-      .data(data)
+  this.svg.selectAll('.bar')
+      .data(this.data)
     .enter().append('rect')
-      .attr('class', 'bar');
-
-
+      .attr('class', 'bar')
+      .attr("x", function(d) { return x(d.label); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return height-y(d.value); })
+      .attr("height", function(d) { return y(d.value); });
 };
